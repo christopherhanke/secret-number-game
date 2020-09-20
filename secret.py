f728bbe3dbc6e0e200b831ec1667d6e83ee8dad9
Christopher Hanke <christopherhanke@googlemail.com>
2020-09-20 20:51:08 +0200

    added comments

--- secret.py
+++ secret.py
@@ -1,12 +1,15 @@
 import random
 
+# initializing
 secret = random.randint(1, 100)
 attempts = 0
 guess = -1
 score_avaible = True
 
+# welcome on screen
 print("\n\nHello and welcome to the secret number game.")
 
+# try to open the score or going first player
 try:
     with open("score.csv", "r") as score_data:
         score = score_data.read()
@@ -15,6 +18,7 @@ except FileNotFoundError:
     print("Your the first player and will start a new score table.")
     score_avaible = False
 
+# loop the guesses till secret number is found
 while guess != secret:
     if attempts == 0:
         try:
@@ -29,20 +33,23 @@ while guess != secret:
             print("Please enter a valid guess. (Number between 1 and 100)")
             continue
     
+    # iterate attempts
     attempts += 1
 
+    # hint the secret
     if guess < secret:
         print("Your guess is below the secret number.\n")
     if secret < guess:
         print("Your guess is above the secret number.\n")
 
+# secret number achieved
 print("\n\nHooray, you found it!")
 print(f"The secret number is {secret} and you guessed {attempts} times.")
 
-
+# writing to the score data
 if score_avaible:
     with open("score.csv", "a") as score_data:
-        score_data.write(f", {str(attempts)}")
+        score_data.write(f",{str(attempts)}")
 else:
     with open("score.csv", "w") as score_data:
-        score_data.write(f", {str(attempts)}")
+        score_data.write(f",{str(attempts)}")

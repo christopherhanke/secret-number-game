1baca673030d5fc6da93c31a1836ef04a4fa7ee7
Christopher Hanke <christopherhanke@googlemail.com>
2020-09-23 21:56:45 +0200

    added more data to saving and set up a top score display

--- secret.py
+++ secret.py
@@ -2,16 +2,24 @@ import random
 import json
 
 from datetime import datetime
+from operator import itemgetter
 
 # initializing
 secret = random.randint(1, 100)
 attempts = 0
 guess = -1
+guesses = []
 score = []
 score_avaible = True
+question = None
 
 # welcome on screen
-print("\n\nHello and welcome to the secret number game.")
+print("\n\nWelcome to the secret number game.")
+
+#login with user name
+name = input("Please login with your name: ")
+print(f"\nHello {name}, great to see you.")
+
 
 # try to open the score or going first player
 try:
@@ -24,10 +32,22 @@ except FileNotFoundError:
         score_file.write("[]")
         score_avaible = False
 
+
+# show score list, if player wants it
 if score_avaible:
-    print("The users before you, left this scores.")
-    for data in score:
-        print(f"{data.get('attempts')} Attempts at {data.get('date')}.")
+    while question != "y" and question !="n":
+        question = input("Do you wanna see the top three scores? [y/n] ")
+        question = question.lower()
+
+    if question == "y":
+        #sorting score list using players attempts
+        best_list = sorted(score, key=itemgetter('attempts'))
+        
+        #printing top three scores of attempts
+        print("Top Three Scores")
+        for data in best_list[:3]:
+            print(f"{data.get('user')} needed {data.get('attempts')} Attempts, at {data.get('date')}.")
+
 
 # loop the guesses till secret number is found
 while guess != secret:
@@ -47,19 +67,25 @@ while guess != secret:
     # iterate attempts
     attempts += 1
 
+    #save guesses
+    guesses.append(guess)
+
     # hint the secret
     if guess < secret:
         print("Your guess is below the secret number.\n")
     if secret < guess:
         print("Your guess is above the secret number.\n")
 
-# secret number achieved
+
+# secret number achieved and game ending
 print("\n\nHooray, you found it!")
 print(f"The secret number is {secret} and you guessed {attempts} times.")
 
-new_score = {"attempts": attempts, "date": str(datetime.now())}
+
+# saving score
+new_score = {"attempts": attempts, "user": name, "date": str(datetime.now()), "secret": secret, "guesses": guesses}
 score.append(new_score)
 
-# writing to the score data
+# saving to file
 with open("score.txt", "w") as score_file:
     score_file.write(json.dumps(score))
